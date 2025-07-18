8b9aa8c883da37724a8d460b645a2aaab7db9b73
shadow <email2agastya@gmail.com>
2025-07-17 20:07:12 -0700

    Update 07_aboutme.py

--- pages/07_aboutme.py
+++ pages/07_aboutme.py
@@ -6,10 +6,10 @@ with tab1:
         st.image(r"light_fruit.webp",width=200)
         st.subheader("Agastya has a light fruit.💫")
     with col2:
-        st.write("Agastya is a really good ggrinder in the Roblox game, Blox Fruits. He loves the game and has a pretty good status " \
+        st.write("Agastya is a really good grinder in the Roblox game, Blox Fruits. He loves the game and has a pretty good status " \
         "and would like to help any of you who would like to grind like him too.")
 with tab2:
     st.write("I love to do coding, playing piano, playing tennis, and of course playing video games. I also like physical stuff.")
 with tab3:
     st.write("email: email2agastya@gmail.com")
-    st.write("roblox profile: rip_Jax")
+    st.write("roblox profile: Foltynisdabest")

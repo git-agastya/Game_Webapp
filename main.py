0c383e94a147903f908829d0a4fb50870aa5f95b
shadow <email2agastya@gmail.com>
2025-07-17 19:38:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("Game Fun App")
-st.header("Welcom to the Best Game Webapp!",divider="rainbow")
+st.header("Welcome to the Best Game Webapp!",divider="rainbow")
 st.write("Choose what pages you want to go to in the sidebar on your left!")

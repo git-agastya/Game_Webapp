99b3fa9959fd2cc92d156d624c8db15b500aba41
shadow <email2agastya@gmail.com>
2025-07-17 19:42:58 -0700

    Update 07_aboutme.py

--- pages/07_aboutme.py
+++ pages/07_aboutme.py
@@ -3,7 +3,7 @@ tab1,tab2,tab3=st.tabs(["About","Hobbies","Contact"])
 with tab1:
     col1,col2=st.columns([0.3,0.7])
     with col1:
-        st.image(r"pages\light_fruit.webp",width=200)
+        st.image(r"light_fruit.webp",width=200)
         st.subheader("Agastya has a light fruit.💫")
     with col2:
         st.write("Agastya is a really good ggrinder in the Roblox game, Blox Fruits. He loves the game and has a pretty good status " \
